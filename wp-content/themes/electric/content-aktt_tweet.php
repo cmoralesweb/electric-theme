92faaf7c1fa768c986626d5a48de2d20d76a8f86
Carlos Morales <carlos@cmorales.es>
2013-09-07 02:02:48 +0200

    Improved tweets display

--- wp-content/themes/electric/content-aktt_tweet.php
+++ wp-content/themes/electric/content-aktt_tweet.php
@@ -5,18 +5,24 @@
             <div class="media-object-container"><?php electric_the_twitter_avatar($raw_tweet); ?></div>
             <div class="media-body"><?php the_content() ?></div>
         </div>
-        <?php electric_the_thumbnail(  ) ?>
+
+        <?php $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large'); ?>
+        <div class="thumbnail-wrapper">
+            <a href="<?php echo $large_image_url[0] ?>" title="<?php the_title_attribute()?>" class="thumbnail" >
+                <?php echo get_the_post_thumbnail($post->ID, 'medium'); ?>
+            </a>
+        </div>
         <?php electric_the_twitter_meta($raw_tweet) ?>
     </div>
     <?php if (!is_search() && !is_archive()) : ?>
-        <?php comments_template('', true); ?>
-    <?php else: ?>
-     <footer class="entry-meta">
-        <a class="more-link" href="<?php esc_attr(the_permalink()) ?>">
-             <?php _e('Read more ', 'electric')?>
-         </a>
-     </footer>
-    <?php endif; ?>
+    <?php comments_template('', true); ?>
+<?php else: ?>
+ <footer class="entry-meta">
+    <a class="more-link" href="<?php esc_attr(the_permalink()) ?>">
+     <?php _e('Read more ', 'electric')?>
+ </a>
+</footer>
+<?php endif; ?>
 </article>
 <?php if (!is_search() && !is_archive()) : ?>
     <?php electric_content_nav("nav-single") ?>
